f22c88ad88ebc7812ff8b8ce65655912303c2b69
Vamsi <bpadala@stevens.edu>
2024-11-12 13:42:30 -0500

    Initial Commit

---
+++ QuickBite/QuickBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBiteApp.swift
+//  QuickBite
+//
+//  Created by Bhaskara Padala on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickBiteTests/QuickBiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBiteTests.swift
+//  QuickBiteTests
+//
+//  Created by Bhaskara Padala on 11/12/24.
+//
+
+import Testing
+@testable import QuickBite
+
+struct QuickBiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
