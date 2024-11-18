6a68a0824bbf42e43d25d28e0302f5b5e97adea9
Vamsi <bpadala@stevens.edu>
2024-11-17 19:04:09 -0500

    User

---
+++ QuickBite/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  QuickBite
+//
+//  Created by Bhaskara Padala on 11/17/24.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
